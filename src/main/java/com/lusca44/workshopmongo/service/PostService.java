41f909fd0f2d42bc577d8dae6a19d3878d16021b
Lusca44 <lucasssrodrigues1@gmail.com>
2021-08-23 21:07:33 -0300

    create postService and postResource

---
+++ src/main/java/com/lusca44/workshopmongo/service/PostService.java
@@ -0,0 +1,24 @@
+package com.lusca44.workshopmongo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lusca44.workshopmongo.domain.Post;
+import com.lusca44.workshopmongo.repository.PostRepository;
+import com.lusca44.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
